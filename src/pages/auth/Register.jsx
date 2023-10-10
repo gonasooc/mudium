9f93e1549fefc5a63a681e78d41ad64e3d1cf5a0
gonasooc <doupler@gmail.com>
2023-10-10 15:21:14 +0900

    Style: 회원가입 스타일 변경

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -59,59 +59,65 @@ function Register() {
   return (
     <div className="page-container">
       <div className="layout-center">
-        <div>
-          <TextField
-            required
-            id="standard-basic"
-            label="이메일"
-            name="email"
-            type="email"
-            variant="standard"
-            value={email}
-            onChange={(event) => {
-              onChange(event);
-            }}
-          />
-        </div>
-        <div>
-          <TextField
-            name="password"
-            type="password"
-            value={password}
-            required
-            onChange={(event) => {
-              onChange(event);
-            }}
-            id="standard-basic"
-            label="비밀번호"
-            variant="standard"
-          />
-        </div>
-        {/* TODO: 비밀번호확인 validation 작업 필요 */}
-        <div>
-          <TextField
-            name="password_check"
-            type="password"
-            value={passwordCheck}
-            required
-            onChange={(event) => {
-              onChange(event);
-            }}
-            id="standard-basic"
-            label="비밀번호 확인"
-            variant="standard"
-          />
-        </div>
-
-        <div style={{ padding: "10px 0" }}>
-          <Button
-            variant="outlined"
-            onClick={() => {
-              register();
-            }}
-          >
-            회원가입
-          </Button>
+        <div className="panel">
+          <div>
+            <TextField
+              fullWidth
+              required
+              id="standard-basic"
+              label="이메일"
+              name="email"
+              type="email"
+              variant="standard"
+              value={email}
+              onChange={(event) => {
+                onChange(event);
+              }}
+            />
+          </div>
+          <div>
+            <TextField
+              fullWidth
+              name="password"
+              type="password"
+              value={password}
+              required
+              onChange={(event) => {
+                onChange(event);
+              }}
+              id="standard-basic"
+              label="비밀번호"
+              variant="standard"
+            />
+          </div>
+          {/* TODO: 비밀번호확인 validation 작업 필요 */}
+          <div>
+            <TextField
+              fullWidth
+              name="password_check"
+              type="password"
+              value={passwordCheck}
+              required
+              onChange={(event) => {
+                onChange(event);
+              }}
+              id="standard-basic"
+              label="비밀번호 확인"
+              variant="standard"
+            />
+          </div>
+          <div style={{ padding: "10px 0" }}>
+            <Button
+              fullWidth
+              size="large"
+              variant="outlined"
+              onClick={() => {
+                register();
+              }}
+            >
+              회원가입
+            </Button>
+          </div>
         </div>
       </div>
     </div>
